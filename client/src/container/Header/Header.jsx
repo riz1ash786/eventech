c0f576c0bebf650921dd7fc3ba03abcdae207b1c
Nadine <nadine.mohsen@hotmail.com>
2022-06-09 19:13:03 +0100

    header

---
+++ client/src/container/Header/Header.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+
+import conference from "../../assets/conference.png";
+import arrow from "../../assets/arrow.png";
+import "./Header.css";
+
+const Header = () => (
+  <div className="custom-header custom-wrap padding-section" id="home">
+    <div className="info-wrap">
+      <div>
+        <p className="subtitle">Stay up-to-date with the latest</p>
+        <img src={arrow} alt="arrow_image" className="arrow-img" />
+      </div>
+      <h1 className="custom-header-h1">Tech Events & Conferences</h1>
+      <p className="small-parag" style={{ margin: "2rem 0" }}>
+        An app where you can find all the upcoming tech events and conferences,
+        create your own event and more ...
+      </p>
+      <button type="button" className="custom-btn">
+        Explore Upcoming Events
+      </button>
+    </div>
+
+    {/* <div className="img-wrap">
+      <img src={conference} alt="header_img" />
+    </div> */}
+  </div>
+);
+
+export default Header;
