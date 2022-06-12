1053102a83b179b797183c7600c388e472c53034
Rizwan Ashraf <riz1ash786@gmail.com>
2022-06-12 15:23:03 +0100

    profile model fixed

--- server/models/Profile.js
+++ server/models/Profile.js
@@ -1,43 +1,43 @@
-// const { Schema, model } = require('mongoose');
-// const bcrypt = require('bcrypt');
-// const Interested = require("./Interested");
+const { Schema, model } = require("mongoose");
+const bcrypt = require("bcrypt");
+const Interested = require("./Interested");
 
-// const profileSchema = new Schema({
-//   name: {
-//     type: String,
-//     required: true,
-//     unique: true,
-//     trim: true,
-//   },
-//   email: {
-//     type: String,
-//     required: true,
-//     unique: true,
-//     match: [/.+@.+\..+/, 'Must match an email address!'],
-//   },
-//   password: {
-//     type: String,
-//     required: true,
-//     minlength: 5,
-//   },
-//   allinterested: [Interested.schema]
-// });
+const profileSchema = new Schema({
+  name: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    match: [/.+@.+\..+/, "Must match an email address!"],
+  },
+  password: {
+    type: String,
+    required: true,
+    minlength: 5,
+  },
+  allinterested: [Interested.schema],
+});
 
-// // set up pre-save middleware to create password
-// profileSchema.pre('save', async function (next) {
-//   if (this.isNew || this.isModified('password')) {
-//     const saltRounds = 10;
-//     this.password = await bcrypt.hash(this.password, saltRounds);
-//   }
+// set up pre-save middleware to create password
+profileSchema.pre("save", async function (next) {
+  if (this.isNew || this.isModified("password")) {
+    const saltRounds = 10;
+    this.password = await bcrypt.hash(this.password, saltRounds);
+  }
 
-//   next();
-// });
+  next();
+});
 
-// // compare the incoming password with the hashed password
-// profileSchema.methods.isCorrectPassword = async function (password) {
-//   return bcrypt.compare(password, this.password);
-// };
+// compare the incoming password with the hashed password
+profileSchema.methods.isCorrectPassword = async function (password) {
+  return bcrypt.compare(password, this.password);
+};
 
-// const Profile = model('Profile', profileSchema);
+const Profile = model("Profile", profileSchema);
 
-// module.exports = Profile;
+module.exports = Profile;
