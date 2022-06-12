5efacc6dc0700e622731262f67e415ce65dd0261
Joe Curtis <joecurtis9999@gmail.com>
2022-06-12 19:51:34 +0100

    login backend

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -8,82 +8,82 @@ const Login = () => {
 const [formState, setFormState] = useState({ email: '', password: '' });
 const [login, { error, data }] = useMutation(LOGIN_USER);
 
-// // update state based on form input changes
-// const handleChange = (event) => {
-//   const { name, value } = event.target;
+// update state based on form input changes
+const handleChange = (event) => {
+  const { name, value } = event.target;
 
-//   setFormState({
-//     ...formState,
-//     [name]: value,
-//   });
-// };
+  setFormState({
+    ...formState,
+    [name]: value,
+  });
+};
 
-// // submit form
-// const handleFormSubmit = async (event) => {
-//   event.preventDefault();
-//   console.log(formState);
-//   try {
-//     const { data } = await login({
-//       variables: { ...formState },
-//     });
+// submit form
+const handleFormSubmit = async (event) => {
+  event.preventDefault();
+  console.log(formState);
+  try {
+    const { data } = await login({
+      variables: { ...formState },
+    });
 
-//     Auth.login(data.login.token);
-//   } catch (e) {
-//     console.error(e);
-//   }
+    Auth.login(data.login.token);
+  } catch (e) {
+    console.error(e);
+  }
 
-//   // clear form values
-//   setFormState({
-//     email: '',
-//     password: '',
-//   });
-// };
+  // clear form values
+  setFormState({
+    email: '',
+    password: '',
+  });
+};
 
 return (
-  <main>
-    <div>
-      <div>
-        <h4>Login</h4>
-        <div>
-          {data ? (
-            <p>
-              Success! You may now head{' '}
-              <Link to="/">back to the homepage.</Link>
-            </p>
-          ) : (
-            <form onSubmit={handleFormSubmit}>
-              <input
-                placeholder="Your email"
-                name="email"
-                type="email"
-                value={formState.email}
-                onChange={handleChange}
-              />
-              <input
-                placeholder="******"
-                name="password"
-                type="password"
-                value={formState.password}
-                onChange={handleChange}
-              />
-              <button
-                style={{ cursor: 'pointer' }}
-                type="submit"
-              >
-                Submit
-              </button>
-            </form>
-          )}
+  // <main>
+  //   <div>
+  //     <div>
+  //       <h4>Login</h4>
+  //       <div>
+  //         {data ? (
+  //           <p>
+  //             Success! You may now head{' '}
+  //             <Link to="/">back to the homepage.</Link>
+  //           </p>
+  //         ) : (
+  //           <form onSubmit={handleFormSubmit}>
+  //             <input
+  //               placeholder="Your email"
+  //               name="email"
+  //               type="email"
+  //               value={formState.email}
+  //               onChange={handleChange}
+  //             />
+  //             <input
+  //               placeholder="******"
+  //               name="password"
+  //               type="password"
+  //               value={formState.password}
+  //               onChange={handleChange}
+  //             />
+  //             <button
+  //               style={{ cursor: 'pointer' }}
+  //               type="submit"
+  //             >
+  //               Submit
+  //             </button>
+  //           </form>
+  //         )}
 
-          {error && (
-            <div>
-              {error.message}
-            </div>
-          )}
-        </div>
-      </div>
-    </div>
-  </main>
+  //         {error && (
+  //           <div>
+  //             {error.message}
+  //           </div>
+  //         )}
+  //       </div>
+  //     </div>
+  //   </div>
+  // </main>
 );
 };
 
