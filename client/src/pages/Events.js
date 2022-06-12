13b60926556ea987770e246c49cbce06612c8720
Joe Curtis <joecurtis9999@gmail.com>
2022-06-12 19:52:37 +0100

    displaying events data

--- client/src/pages/Events.js
+++ client/src/pages/Events.js
@@ -1,46 +1,46 @@
-// import React, { useState } from 'react';
-// import { useQuery } from '@apollo/client';
-// import { QUERY_ALL_EVENTS } from '../utils/queries';
+import React, { useState } from 'react';
+import { useQuery } from '@apollo/client';
+import { QUERY_ALL_EVENTS } from '../utils/queries';
 
-// const Events = () => {
-//     const { loading, data } = useQuery(QUERY_ALL_EVENTS);
-//     const events = data?.events || [];
+const Events = () => {
+    const { loading, data } = useQuery(QUERY_ALL_EVENTS);
+    const events = data?.events || [];
 
-//     if (!events.length) {
-//         return <h3>No Events Yet</h3>;
-//       }
+    if (!events.length) {
+        return <h3>No Events Yet</h3>;
+      }
 
-    return(
+//     return(
      
-        <div>
-          <div>
-            {loading ? (
-              <div>Loading...</div>
-            ) : (
-              <>
-              {events&& 
-              events.map((oneevent)=> (
-                  <>
-                  <div>
-                      {oneevent.name}
-                      {oneevent.description}
-                      {oneevent.whyattend}
-                      {oneevent.quantity}
-                      {oneevent.price}
-                      {oneevent.location.name}
-                      <img src={`/images/${oneevent.image}`}/>
+//         <div>
+//           <div>
+//             {loading ? (
+//               <div>Loading...</div>
+//             ) : (
+//               <>
+//               {events&& 
+//               events.map((oneevent)=> (
+//                   <>
+//                   <div>
+//                       {oneevent.name}
+//                       {oneevent.description}
+//                       {oneevent.whyattend}
+//                       {oneevent.quantity}
+//                       {oneevent.price}
+//                       {oneevent.location.name}
+//                       <img src={`/images/${oneevent.image}`}/>
                      
-                      </div>
-                  </>
-              ))}
-              </>
+//                       </div>
+//                   </>
+//               ))}
+//               </>
                  
-            )}
-          </div>
-        </div>
+//             )}
+//           </div>
+//         </div>
  
-)
-};
+// )
+// };
   
-export default Events;
+// export default Events;
   
