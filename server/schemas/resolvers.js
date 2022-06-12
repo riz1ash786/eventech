843b6e7ca5be50f9a6f9725ca95f61045308c168
Rizwan Ashraf <riz1ash786@gmail.com>
2022-06-12 15:18:52 +0100

    mutations for profile

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -67,29 +67,29 @@ const resolvers = {
     },
   },
 
-//   Mutation: {
-//     addProfile: async (parent, { name, email, password }) => {
-//       const profile = await Profile.create({ name, email, password });
-//       const token = signToken(profile);
+  Mutation: {
+    addProfile: async (parent, { name, email, password }) => {
+      const profile = await Profile.create({ name, email, password });
+      const token = signToken(profile);
 
-//       return { token, profile };
-//     },
-//     login: async (parent, { email, password }) => {
-//       const profile = await Profile.findOne({ email });
+      return { token, profile };
+    },
+    login: async (parent, { email, password }) => {
+      const profile = await Profile.findOne({ email });
 
-//       if (!profile) {
-//         throw new AuthenticationError('No profile with this email found!');
-//       }
+      if (!profile) {
+        throw new AuthenticationError('No profile with this email found!');
+      }
 
-//       const correctPw = await profile.isCorrectPassword(password);
+      const correctPw = await profile.isCorrectPassword(password);
 
-//       if (!correctPw) {
-//         throw new AuthenticationError('Incorrect password!');
-//       }
+      if (!correctPw) {
+        throw new AuthenticationError('Incorrect password!');
+      }
 
-//       const token = signToken(profile);
-//       return { token, profile };
-//     },
+      const token = signToken(profile);
+      return { token, profile };
+    },
 
 
 //     // Set up mutation so a logged in user can only remove their profile and no one else's
