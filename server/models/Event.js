940bf886c1741dc69eef4b6466a29898a5a275e1
Rizwan Ashraf <riz1ash786@gmail.com>
2022-06-12 15:21:52 +0100

    interested model

--- server/models/Event.js
+++ server/models/Event.js
@@ -1,39 +1,39 @@
-// const mongoose = require("mongoose");
+const mongoose = require("mongoose");
 
-// const { Schema } = mongoose;
+const { Schema } = mongoose;
 
-// const eventSchema = new Schema({
-//   name: {
-//     type: String,
-//     required: true,
-//     trim: true,
-//   },
-//   description: {
-//     type: String,
-//   },
-//   whyattend: {
-//     type: String,
-//   },
-//   image: {
-//     type: String,
-//   },
-//   price: {
-//     type: Number,
-//     required: true,
-//     min: 0.99,
-//   },
-//   quantity: {
-//     type: Number,
-//     min: 0,
-//     default: 0,
-//   },
-//   location: {
-//     type: Schema.Types.ObjectId,
-//     ref: "Location",
-//     required: true,
-//   },
-// });
+const eventSchema = new Schema({
+  name: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  description: {
+    type: String,
+  },
+  whyattend: {
+    type: String,
+  },
+  image: {
+    type: String,
+  },
+  price: {
+    type: Number,
+    required: true,
+    min: 0.99,
+  },
+  quantity: {
+    type: Number,
+    min: 0,
+    default: 0,
+  },
+  location: {
+    type: Schema.Types.ObjectId,
+    ref: "Location",
+    required: true,
+  },
+});
 
-// const Event = mongoose.model("Event", eventSchema);
+const Event = mongoose.model("Event", eventSchema);
 
-// module.exports = Event;
+module.exports = Event;
