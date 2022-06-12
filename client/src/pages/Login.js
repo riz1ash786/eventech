c9503654d1aec0e22672d70cd597482c1cba582a
Joe Curtis <joecurtis9999@gmail.com>
2022-06-12 19:50:33 +0100

    login page created, frontend

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -8,36 +8,36 @@ const Login = () => {
 const [formState, setFormState] = useState({ email: '', password: '' });
 const [login, { error, data }] = useMutation(LOGIN_USER);
 
-// update state based on form input changes
-const handleChange = (event) => {
-  const { name, value } = event.target;
+// // update state based on form input changes
+// const handleChange = (event) => {
+//   const { name, value } = event.target;
 
-  setFormState({
-    ...formState,
-    [name]: value,
-  });
-};
+//   setFormState({
+//     ...formState,
+//     [name]: value,
+//   });
+// };
 
-// submit form
-const handleFormSubmit = async (event) => {
-  event.preventDefault();
-  console.log(formState);
-  try {
-    const { data } = await login({
-      variables: { ...formState },
-    });
+// // submit form
+// const handleFormSubmit = async (event) => {
+//   event.preventDefault();
+//   console.log(formState);
+//   try {
+//     const { data } = await login({
+//       variables: { ...formState },
+//     });
 
-    Auth.login(data.login.token);
-  } catch (e) {
-    console.error(e);
-  }
+//     Auth.login(data.login.token);
+//   } catch (e) {
+//     console.error(e);
+//   }
 
-  // clear form values
-  setFormState({
-    email: '',
-    password: '',
-  });
-};
+//   // clear form values
+//   setFormState({
+//     email: '',
+//     password: '',
+//   });
+// };
 
 return (
   <main>
