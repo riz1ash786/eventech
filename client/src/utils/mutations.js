56c5f14180493ae0fe7f853da3175e9efc48ba49
Joe Curtis <joecurtis9999@gmail.com>
2022-06-12 19:55:58 +0100

    add profile

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -1,16 +1,16 @@
 import { gql } from '@apollo/client';
 
-export const ADD_PROFILE = gql`
-  mutation addProfile($name: String!, $email: String!, $password: String!) {
-    addProfile(name: $name, email: $email, password: $password) {
-      token
-      profile {
-        _id
-        name
-      }
-    }
-  }
-`;
+// export const ADD_PROFILE = gql`
+//   mutation addProfile($name: String!, $email: String!, $password: String!) {
+//     addProfile(name: $name, email: $email, password: $password) {
+//       token
+//       profile {
+//         _id
+//         name
+//       }
+//     }
+//   }
+// `;
 
 
 export const LOGIN_USER = gql`
