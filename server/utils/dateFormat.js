eaff4c8e257f0b8d8e9ad87df60f8d1b68d2d78b
Joe Curtis <joecurtis9999@gmail.com>
2022-06-14 20:08:12 +0100

    date format

--- server/utils/dateFormat.js
+++ server/utils/dateFormat.js
@@ -4,15 +4,15 @@ const addDateSuffix = (date) => {
   // get last char of date string
   const lastChar = dateStr.charAt(dateStr.length - 1);
 
-  if (lastChar === '1' && dateStr !== '11') {
-    dateStr = `${dateStr}st`;
-  } else if (lastChar === '2' && dateStr !== '12') {
-    dateStr = `${dateStr}nd`;
-  } else if (lastChar === '3' && dateStr !== '13') {
-    dateStr = `${dateStr}rd`;
-  } else {
-    dateStr = `${dateStr}th`;
-  }
+  // if (lastChar === '1' && dateStr !== '11') {
+  //   dateStr = `${dateStr}st`;
+  // } else if (lastChar === '2' && dateStr !== '12') {
+  //   dateStr = `${dateStr}nd`;
+  // } else if (lastChar === '3' && dateStr !== '13') {
+  //   dateStr = `${dateStr}rd`;
+  // } else {
+  //   dateStr = `${dateStr}th`;
+  // }
 
   return dateStr;
 };
