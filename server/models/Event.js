571abfac1b84910a3151087fe1c2aaac590f7f09
Nadine <nadine.mohsen@hotmail.com>
2022-06-16 21:31:28 +0100

    Event model updated

--- server/models/Event.js
+++ server/models/Event.js
@@ -2,50 +2,50 @@ const mongoose = require("mongoose");
 
 const { Schema } = mongoose;
 
-const eventSchema = new Schema({
-  title: {
-    type: String,
-    required: true,
-    trim: true,
+const eventSchema = new Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    description: {
+      type: String,
+    },
+    whyattend: {
+      type: String,
+    },
+    image: {
+      type: String,
+    },
+    price: {
+      type: Number,
+      required: true,
+      min: 0.99,
+    },
+    location: {
+      type: Schema.Types.ObjectId,
+      ref: "Location",
+      required: true,
+    },
+    link: {
+      type: String,
+    },
+    createdAt: {
+      type: String,
+    },
+    comments: [
+      {
+        body: String,
+        author: String,
+      },
+    ],
+    likeCount: {
+      type: Number,
+    },
   },
-  description: {
-    type: String,
-  },
-  whyattend: {
-    type: String,
-  },
-  image: {
-    type: String,
-  },
-  price: {
-    type: Number,
-    required: true,
-    min: 0.99,
-  },
-  location: {
-    type: Schema.Types.ObjectId,
-    ref: "Location",
-    required: true,
-  },
-  link:{
-    type:String,
-  },
-  createdAt:{
-    type:String,
-  },
-  profile: {
-    type: Schema.Types.ObjectId,
-    ref: 'Profile'
-  },
-  comments:[{
-    body: String,
-    name:String,
-    createdAt:String,
-  }],
-  likeCount:{
-    type: Number,
-  }
-});
+  { timestamps: true }
+);
 
 eventSchema.virtual("commentCount").get(function () {
   return this.comments.length;
